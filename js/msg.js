f088ef823f49069a9b7566945b00924367d7958e
Ryolin <manilrabia@gmail.com>
2024-03-28 19:03:44 +0100

    map

---
+++ js/msg.js
@@ -0,0 +1,34 @@
+function show_contact() {
+    document.querySelector('#contact-section').style.display = 'block';
+    document.querySelector('.home').style.display = 'none';
+    document.querySelector('#programs-section').style.display = 'none';
+    document.querySelector('#languages-section').style.display = 'none';
+    document.querySelector('#infos-mobile').style.display = 'none';
+    document.querySelector('nav').style.display = 'none';
+    document.querySelector('footer').style.display = 'none';
+}
+
+function hide_contact() {
+    document.querySelector('#contact-section').removeAttribute('style');
+    document.querySelector('.home').removeAttribute('style');
+    document.querySelector('#programs-section').removeAttribute('style');
+    document.querySelector('#languages-section').removeAttribute('style');
+    document.querySelector('#infos-mobile').removeAttribute('style');
+    document.querySelector('nav').removeAttribute('style');
+    document.querySelector('footer').removeAttribute('style');
+}
+
+
+// start
+document.addEventListener('DOMContentLoaded', () => {
+    // resize
+    window.addEventListener('resize', hide_contact);
+    // show contact
+    let buttons = document.querySelectorAll('.send-msg');
+    buttons.forEach(button => {
+        button.addEventListener('click', show_contact);
+    });
+
+    // hide contact
+    document.querySelector('#go-back').addEventListener('click', hide_contact);
+});
